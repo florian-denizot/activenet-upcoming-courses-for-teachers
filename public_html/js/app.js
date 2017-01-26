9a8a1d24f6f12ee747776affd4138aa5e40f2f4b
Florian Denizot <florian.denizot@gmail.com>
2017-01-26 17:56:23 -0500

    Setup

---
+++ public_html/js/app.js
@@ -0,0 +1,23 @@
+angular.module('activenetupcomingCoursesForTeachers', []).
+  controller('MainController',  ['$scope', '$http', '$sce', function($scope, $http, $sce) { 
+    
+    $scope.courses = [];    
+    
+    var url = 'http://api.amp.active.com/v2/search';
+    url = $sce.trustAsResourceUrl(url);
+    
+    $http.jsonp(url, {jsonpCallbackParam: 'callback', 
+        data:{
+          query:'A1.1',
+          org_id:'6b1423e1-84b8-4d92-8857-65860e5ace46',
+          city:'Toronto',
+          start_date:'2017-01-26..2017-03-30',
+          api_key:'xshzx6dy2mgrghnruj46t9fz'
+        }}).
+      then(function(data){
+        console.log(data.found);
+      }).catch(function(err){
+        console.debug(err);
+      });
+  }]);
+
